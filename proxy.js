100e980be8c04415e2feb2f47e68854d96082352
Rohit Shrivastava <rohit.shrivastava22@gmail.com>
2025-12-21 09:04:28 +0530

    Refactor clerkMiddleware to use publicRoutes

--- proxy.js
+++ proxy.js
@@ -1,14 +1,17 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
-
-export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) await auth.protect();
+export default clerkMiddleware({
+  publicRoutes: [
+    '/',
+    '/pricing',
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+  ],
 });
 
 export const config = {
-    matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        '/(api|trpc)(.*)',
-    ],
+  matcher: [
+    '/((?!_next|.*\\..*).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
