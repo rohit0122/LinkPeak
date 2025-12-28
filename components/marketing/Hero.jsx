3cc6004eac29bb924403eeaf587bdd944d56dd3a
Rohit Shrivastava <rohit.shrivastava22@gmail.com>
2025-12-28 16:54:23 +0530

    Update marketing text in Hero component

--- components/marketing/Hero.jsx
+++ components/marketing/Hero.jsx
@@ -36,7 +36,7 @@ export default function Hero() {
                             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
                             <span className="relative inline-flex rounded-full h-2 w-2 bg-primary"></span>
                         </span>
-                        Next-Gen Bio Links with power of AI
+                        Next-Gen Bio Links.
                     </div>
 
                     <h1 className="text-6xl md:text-8xl font-medium tracking-tighter leading-[0.9] text-base-content mb-8">
