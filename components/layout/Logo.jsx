12ccb4209c84d6abd178c770eb6623074f234a83
Rohit Shrivastava <rohit.shrivastava22@gmail.com>
2026-01-22 19:43:48 +0530

    Fixed svg icons

--- components/layout/Logo.jsx
+++ components/layout/Logo.jsx
@@ -1,11 +1,11 @@
 import { CONFIG } from "@/constants/config";
+import Image from "next/image";
 import Link from "next/link";
-import { PiGraphDuotone } from "react-icons/pi";
 
 export default function Logo() {
     return (
         <Link href="/" className="text-xl font-semibold flex items-center gap-2">
-            <PiGraphDuotone className="w-6 h-6 text-nuetral" />
+            <Image src="/logo.svg" alt="Logo" className="w-6 h-6" width={24} height={24} />
             <span className="text-primary text-2xl font-semibold">
                 {CONFIG.SITE_NAME}
             </span>
